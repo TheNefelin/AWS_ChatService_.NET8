b5b97f0e8689cc33c4df8b785c936cec55500d8d
Francisco Carmona <flcarmonac@yahoo.com>
2025-08-14 13:04:40 -0400

    update

--- Script_AWS_CloudShell.sh
+++ Script_AWS_CloudShell.sh
@@ -10,14 +10,22 @@ docker system prune -a -f
 # Clean Docker volumes (careful, deletes unused volumes)
 docker volume prune -f
 
+# Dlete file or folder if it exists
+rm -rf AWS_ChatService_.NET8
+rm text.txt
+
 # Clone repo
 git clone [REPO_URL]
 
 # Enter project folder
-cd [REPO_NAME]
+cd AWS_ChatService_.NET8
+
+# Check disk space again
+tar -czf context.tar.gz .dockerignore Dockerfile * && du -sh context.tar.gz
 
 # Build Docker image
 docker build -t dotnet-app-repo .
+docker build --no-cache -t dotnet-app-repo .
 
 # Tag Docker image for ECR
 docker tag dotnet-app-repo:latest 123456.dkr.ecr.us-east-1.amazonaws.com/dotnet-app-repo:latest
