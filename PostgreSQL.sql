3dd36b56baf9fc654c2b6ff95e4478ab33ac68e5
Francisco Carmona <flcarmonac@yahoo.com>
2025-08-20 16:46:23 -0400

    update

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS Users (
 CREATE UNIQUE INDEX IF NOT EXISTS IX_Users_Email ON Users(Email);
 CREATE UNIQUE INDEX IF NOT EXISTS IX_Users_GoogleId ON Users(GoogleId);
 
-INSERT INTO Users (Email, GoogleId, Names, ConnectedAt)
+INSERT INTO Users (Email, GoogleId, Names, CreatedAt)
 VALUES (
     'bot@user.ia',
     'google_fake_123456789',
@@ -21,28 +21,39 @@ VALUES (
 );
 
 CREATE TABLE IF NOT EXISTS ChatRooms (
-    Id UUID PRIMARY KEY,
+    Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     Name TEXT NOT NULL,
     CreatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+INSERT INTO ChatRooms 
+    (Name, CreatedAt)
+VALUES (
+    'Sala de Prueba', 
+    CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS Messages (
-    Id UUID PRIMARY KEY,
+    Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     Content TEXT NOT NULL,
     SentAt TIMESTAMP NOT NULL,
     UserId UUID NOT NULL REFERENCES Users(Id),
     ChatRoomId UUID NOT NULL REFERENCES ChatRooms(Id)
 );
 
-
-INSERT INTO ChatRooms (Id, Name, CreatedAt)
-VALUES ('11112222-3333-4444-5555-666677778888', 'Sala de Prueba', CURRENT_TIMESTAMP);
-
 INSERT INTO Messages 
-    (Id, Content, SentAt, UserId, ChatRoomId)
-VALUES
-    ('11111111-3333-4444-5555-666677778888', '¡Hola desde REST!', CURRENT_TIMESTAMP, '11112222-3333-4444-5555-666677778888', '11112222-3333-4444-5555-666677778888'),
-    ('22222222-3333-4444-5555-666677778888', 'Mensaje de prueba desde la API', CURRENT_TIMESTAMP, '11112222-3333-4444-5555-666677778888', '11112222-3333-4444-5555-666677778888');
+    (Content, SentAt, UserId, ChatRoomId)
+VALUES (
+    '¡Hola desde REST!', 
+    CURRENT_TIMESTAMP, 
+    '849e20e0-c5b2-407d-97f3-4e26f7198a37', 
+    '1fb61def-542c-4dfc-9c65-fc772a89fa53'
+);
+
+SELECT 
+	a.id AS UserId,
+	b.id AS ChatRommId
+FROM Users AS a, ChatRooms AS b;
 
 SELECT * FROM Users;
 SELECT * FROM ChatRooms;
